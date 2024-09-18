7be7ecb77c33acd645236baa33b038a1919374b0
Malcontatos <malconroger16@gmail.com>
2024-09-18 20:36:25 -0300

    Add files via upload

---
+++ aual02/aula02.py
@@ -0,0 +1,58 @@
+"""
+1. Solicite o peso e a altura de um usuário, calcule o seu imc e exiba o resultado
+IMC = peso / altura²
+
+2. Solicite ao usuário base e altura de um triangulo, calcule a sua área e exiba o resultado
+AREA = base x altura / 2
+
+3. Solicite ao usuário uma temperatura em celsius e a converta para fahreinheit e exiba o resultado
+F = (celsius x 1.8) + 32  
+
+4. Solicite uma quantidade inteira de horas e a converta para minutos e exiba o resultado
+
+5. Solicite um valor em dolar e converta para real, considere a cotação 5.67
+"""
+"""
+peso = float (input("Digite o peso: "))
+altura = float(input("Digite sua altura em metros: "))
+imc = peso / (altura * altura)
+print (f"SEU IMC É: {imc:.2f} ")
+"""
+"""
+base = float (input ("Digite a base do triangulo: "))
+altura = float (input("Digite a altura do triangulo: "))
+area = (base*altura)/2
+print (f"A área do triangulo é: {area:.2f}")
+"""
+"""
+celsius = float (input("Digite a temperatura em Celsius: "))
+fah =(celsius * 1.8) + 32
+print(f"A temperatura em Celsius é: {celsius}\nEm Fahrenheit é: {fah}")
+"""
+"""
+horas = int (input("Digite a quantidade de horas: "))
+minutos = horas * 60
+print(f"A quantidade de minutos é {minutos}")
+"""
+"""
+dolar = float (input ("Digite um valor em dolar: "))
+real = dolar * 5.46
+print(f"o valor em real é: {real}")
+"""
+"""
+nota1 = float (input("Digite a nota 1: "))
+nota2 = float (input("Digite a nota 2: "))
+nota3 = float (input("Digite a nota 3: "))
+media = (nota1 + nota2 + nota3)/3
+print(f"A média das notas é: {media:.2f}")
+"""
+"""
+horas = int (input ("Digite as horas: "))
+minutos = horas * 60
+print (f"As horas: {horas}, em minutos são: {minutos}")
+"""
+raio = float (input("Digite o raio do circulo: "))
+area = 3.14 * (raio ** 2)
+print (f"A área do circulo é: {area:.2f}")
+
+
