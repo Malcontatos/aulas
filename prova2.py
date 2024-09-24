377019faa1edaaeaccdaee095999a16285551c1b
Malcontatos <malconroger16@gmail.com>
2024-09-23 22:27:08 -0300

    Prova02
    Escreva um programa em python que pergunte ao usuário a velocidade de um carro. Caso ultrapasse 80 km/h, exiba uma mensagem dizendo que o usuário foi multado. Nesse caso, exiba o valor da multa, cobrando R$20,00 por cada km que exceder 80 km/h.

---
+++ prova2.py
@@ -0,0 +1,12 @@
+velocidade = int (input("Qual a velocidade do carro em Km/h ? "))
+print(f'{velocidade}Km/h')
+limite = 80
+
+#CALCULO DA MULTA
+excesso = velocidade - limite
+multa = excesso * 20
+
+if velocidade > 80:
+    print(f"Multado por excesso de velocidade!\n VALOR DA MULTA: R${multa}")
+else:
+    print("Velocidade permitida!")
